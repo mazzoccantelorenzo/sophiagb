b9846030fe54c7f951f4e02fca47337506ed2c81
Lorenzo <mazzolore97@gmail.com>
2026-01-03 16:28:56 +0100

    game: make function for bytecode rapresentation of rom

--- game/main.go
+++ game/main.go
@@ -6,21 +6,26 @@ package main
 //
 // This is used to create a window that displays the emulator
 
+// https://gbdev.io/pandocs/About.html <--- Emulator Bible to consult :)
 import (
 	"fmt"
-	"io/fs"
-	"log"
-	"path/filepath"
-
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"io"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
 )
 
-type Game struct{}
+type Game struct {
+	selectedRow int
+}
 
 type GBAFiles struct {
-	Files []string
-	Name  string
+	Files      []string
+	Name       string
+	isSelected bool
 }
 
 func FindGBAFiles(dir string) (GBAFiles, error) {
@@ -42,19 +47,116 @@ func FindGBAFiles(dir string) (GBAFiles, error) {
 	return result, err
 }
 
+func hexDumpToFile(data []byte, filename string) error {
+	f, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	for i := 0; i < len(data); i += 16 {
+		fmt.Fprintf(f, "%08X  ", i)
+
+		for j := 0; j < 16 && i+j < len(data); j++ {
+			fmt.Fprintf(f, "%02X ", data[i+j])
+		}
+
+		fmt.Fprint(f, " ")
+
+		for j := 0; j < 16 && i+j < len(data); j++ {
+			b := data[i+j]
+			if b >= 32 && b <= 126 {
+				fmt.Fprintf(f, "%c", b)
+			} else {
+				fmt.Fprint(f, ".")
+			}
+		}
+
+		fmt.Fprintln(f)
+	}
+
+	return nil
+}
+
+// Debug functions to understand what a rom is all about in terms of bytecode.
+func hexDump(data []byte, start, length int) {
+	end := start + length
+	if end > len(data) {
+		end = len(data)
+	}
+
+	for i := start; i < end; i += 16 {
+		fmt.Printf("%08X  ", i)
+
+		for j := 0; j < 16 && i+j < end; j++ {
+			fmt.Printf("%02X ", data[i+j])
+		}
+
+		fmt.Printf(" ")
+
+		for j := 0; j < 16 && i+j < end; j++ {
+			b := data[i+j]
+			if b >= 32 && b <= 126 {
+				fmt.Printf("%c", b)
+			} else {
+				fmt.Print(".")
+			}
+		}
+
+		fmt.Println()
+	}
+}
+
 func (g *Game) Update() error {
+
+	gbaFiles, _ := FindGBAFiles("./roms")
+
+	if ebiten.IsKeyPressed(ebiten.KeyArrowDown) {
+		if g.selectedRow < len(gbaFiles.Files)-1 {
+			g.selectedRow++
+		}
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeyArrowUp) {
+		if g.selectedRow > 0 {
+			g.selectedRow--
+		}
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeyEnter) && len(gbaFiles.Files) > 0 {
+		selectedFile := gbaFiles.Files[g.selectedRow]
+		// Here you have selected your game,so we must open it
+		open, err := ebitenutil.OpenFile(selectedFile)
+		data, err := io.ReadAll(open)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		//fmt.Println(data)
+		fmt.Printf("Game: %s\n ", data[0xA0:0xAC]) //It is a standard for gba roms to print the game's name in these bytes.
+		hexDumpToFile(data, "pokemon_dump.txt")    // I've used this to get a proper rapresentation in bytecode of what is a rom all about.
+	}
+
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
+
 	gbaFiles, _ := FindGBAFiles("./roms")
 
-	for index, file := range gbaFiles.Files {
-
-		ebitenutil.DebugPrintAt(screen, file, 5, index+1*10)
+	for i, file := range gbaFiles.Files {
+		prefix := "  "
+		if i == g.selectedRow {
+			prefix = "> " // This is the arrow that shows which game you have selected
+		}
+		ebitenutil.DebugPrintAt(screen, prefix+file, 10, 20+i*20)
 	}
-	ebitenutil.DebugPrint(screen, "Select a game to open")
-	ebitenutil.OpenFile("")
+
+	if len(gbaFiles.Files) == 0 {
+		ebitenutil.DebugPrint(screen, "Nessun file .gb trovato in ./roms")
+	} else {
+		ebitenutil.DebugPrintAt(screen, "Usa freccia in basso e Invio per selezionare", 10, 10)
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -62,9 +164,9 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 }
 
 func main() {
-	fmt.Println("test")
+
 	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, World!")
+	ebiten.SetWindowTitle("Sophiagb")
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
 	}
